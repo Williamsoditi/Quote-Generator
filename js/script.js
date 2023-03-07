b0ed6757b99e5270b8240e9cf0d8e818744ce781
Williams <williamsoditi99@gmail.com>
2023-03-07 16:15:41 +0300

    Feat : Adds functionality - Adds js content to fetch data from quotes api

---
+++ js/script.js
@@ -0,0 +1,12 @@
+const generateQuote = () => {
+    let url = "https://type.fit/api/quotes";
+    fetch(url).then(function (response) {
+        return response.json()
+    }).then(function (data){
+        let randomNumber = Math.floor((Math.random() * 1500) + 1);
+        let randomQuote = data[randomNumber];
+
+        document.getElementById("quote").innerHTML = `"${randomQuote.text}"`;
+        document.getElementById("author").innerHTML = `~ ${randomQuote.author ? randomQuote.author : ""}`;
+    })
+}
